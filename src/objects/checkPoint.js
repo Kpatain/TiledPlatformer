de58ceac724380695f20224fb4fcc014515b994c
Kpatain <62848924+Kpatain@users.noreply.github.com>
2021-04-05 14:48:37 +0200

    checkpoint ?

---
+++ src/objects/checkPoint.js
@@ -0,0 +1,31 @@
+class checkPoint extends Phaser.Physics.Arcade.Sprite
+{
+    constructor(scene, x, y, image, value)
+    {
+        super(scene, x, y, image);
+        this.scene=scene;
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+        this.valuePos = value;
+        this.body.allowGravity=false;
+        console.log(this.valuePos);
+    }
+
+    savePos()
+    {
+        console.log(this.valuePos);
+        localStorage.setItem('cP', this.valuePos);
+    }
+
+    loadPos()
+    {
+        if (localStorage.getItem('cP') == this.valuePos)
+        {
+            return{
+                x : this.x,
+                y : this.y
+            }
+        }
+        return false;
+    }
+} 
